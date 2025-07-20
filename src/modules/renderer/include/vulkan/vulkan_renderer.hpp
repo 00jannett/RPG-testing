b57a3135b488b2e0e90278284a8faa3c6b06d2ec
00jannett <dontwritetothismail13@gmail.com>
2025-07-20 21:05:22 +0300

    Improvements

--- src/modules/renderer/include/vulkan/vulkan_renderer.hpp
+++ src/modules/renderer/include/vulkan/vulkan_renderer.hpp
@@ -1,18 +1,44 @@
 #ifndef VULKAN_RENDERER_HPP
 #define VULKAN_RENDERER_HPP
 
+//#include "core/include/window/window.hpp"
+#include <window.hpp>
 #include <vulkan/vulkan.h>
 #include <vector>
 #include <string>
 
 class VulkanRenderer : public Renderer {
 public:
-    VulkanRenderer();
+    VulkanRenderer(const std::vector<std::string>& extensions, Window& window);
     ~VulkanRenderer() override;
 
     void init(Window& window) override;
     void drawFrame() override;
     void cleanup() override;
+
+private:
+    // vulkan objects
+    VkInstance instance{};
+    VkPhysicalDevice physicalDevice{};
+    VkDevice device{};
+    VkQueue graphicsQueue{};
+    VkSurfaceKHR surface{};
+    VkSwapchainKHR swapChain{};
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+
+    // methods for initialization
+    void createInstance();
+    void pickPhysicalDevice();
+    void createLogicalDevice();
+    void createSurface(Window& window);
+    void createSwapChain(Window& window);
+    void createRenderPass();    // added on July 20, 2025
+    void createGraphicsPipeline();
+    void createFramebuffers();
+    void createCommandPool();
+    void createCommandBuffers();
 };
 
 #endif // VULKAN_RENDERER_HPP
--- src/modules/renderer/src/vulkan/vulkan_renderer.cpp
+++ src/modules/renderer/src/vulkan/vulkan_renderer.cpp
@@ -1,24 +1,30 @@
-#include "include/vulkan_renderer.hpp"
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 #include <stdexcept>
 #include <vector>
+#include "vulkan_renderer.hpp"
 
 VulkanRenderer::VulkanRenderer(const std::vector<std::string>& extensions)
-: m_Instanse(extensions),
-  m_PhysicalDevice(m_Instanse.Get()),
+: m_Instance(extensions),
+  m_PhysicalDevice(m_Instance.Get()),
   m_Device(m_PhysicalDevice.Get())
 {
-    init();
+    init(window);
 }
 
 VulkanRenderer::~VulkanRenderer() {
     cleanup();
 }
 
-void VulkanRenderer::init() {
-    createSurface();
+void VulkanRenderer::init(Window& window) {
+    createSurface(window);
     pickPhysicalDevice();
     createLogicalDevice();
-    createSwapChain();
+    createSwapChain(window);
     createRenderPass();
     createGraphicsPipeline();
     createFramebuffers();
@@ -26,7 +32,50 @@ void VulkanRenderer::init() {
     createCommandBuffers();
 }
 
+
+void drawFrame() {
+
+}
+
 void VulkanRenderer::cleanup() {
     vkDeviceWaitIdle(m_Device.Get());
 
+    if (m_CommandBuffers.size() > 0) {
+        vkFreeCommandBuffers(m_Device.Get(), m_CommandPool, static_cast<uint32_t>(m_CommandBuffers.size()), m_CommandBuffers.data());
+        m_CommandBuffers.clear();
+    }
+
+    for (auto framebuffer : m_Framebuffers) {
+        vkDestroyFramebuffer(m_Device.Get(), framebuffer, nullptr);
+    }
+    m_Framebuffers.clear();
+
+    vkDestroyPipeline(m_Device.Get(), m_GraphicsPipeline, nullptr);
+    vkDestroyPipelineLayout(m_Device.Get(), m_PipelineLayout, nullptr);
+    vkDestroyRenderPass(m_Device.Get(), m_RenderPass, nullptr);
+
+    for (auto imageView : m_SwapChainImageViews) {
+        vkDestroyImageView(m_Device.Get(), imageView, nullptr);
+    }
+    m_SwapChainImageViews.clear();
+
+    if (m_SwapChain) {
+        vkDestroySwapchainKHR(m_Device.Get(), m_SwapChain, nullptr);
+        m_SwapChain = VK_NULL_HANDLE;
+    }
+
+    if (m_Surface) {
+        vkDestroySurfaceKHR(m_Instance.Get(), m_Surface, nullptr);
+        m_Surface = VK_NULL_HANDLE;
+    }
+
+    if (m_Device) {
+        vkDestroyDevice(m_Device.Get(), nullptr);
+        m_Device = VK_NULL_HANDLE;
+    }
+
+    if (m_CommandPool != VK_NULL_HANDLE) {
+        vkDestroyCommandPool(m_Device.Get(), m_CommandPool, nullptr);
+        m_CommandPool = VK_NULL_HANDLE;
+    }
 }
